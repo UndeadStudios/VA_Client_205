85ff4424cf51cc07367fb4d0438d897be679e917
BuildTools <unconfigured@null.spigotmc.org>
2022-06-22 16:23:31 -0700

    fixed the lighting on the client for objects

--- src/ObjectDefinition.java
+++ src/ObjectDefinition.java
@@ -498,7 +498,8 @@ public final class ObjectDefinition {
 			model_3.method478(anInt748, anInt740, anInt772);
 		if (flag2)
 			model_3.method475(anInt738, anInt745, anInt783);
-		model_3.method479(74, 1000, -90, -580, -90, !aBoolean769);
+		model_3.method479(85 + aByte737, 768 + aByte742 * 25, -50, -10, -50,
+				 !aBoolean769);
 		if (anInt760 == 1)
 			model_3.itemDropHeight = model_3.modelHeight;
 		mruNodes2.removeFromCache(model_3, l1);
